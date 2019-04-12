d8ded5308220d4240e370f7e4c4b60232bff79e8
Sunny Anzalone <saneenaanzalone@gmail.com>
2019-04-12 14:32:43 -0400

    Initial commit

---
+++ starter.py
@@ -0,0 +1,77 @@
+# let's get started sisters
+
+from graphics import *
+
+def starter():
+
+    win = GraphWin("Which European Monarch are you?", 600, 600)
+
+    win.setBackground("lavender")
+
+    firstImg = Image(Point(300,300),"fallofrome2.gif")
+    firstImg.draw(win)
+
+    txt = Text(Point(280, 155), "Which European Monarch are you?")
+    txt.setFace("helvetica")
+    txt.setSize(20)
+    txt.draw(win)
+
+    txt2 = Text(Point (280, 440), "Take this quiz to find out!")
+    txt2.setSize(10)
+    txt2.setStyle
+    txt2.draw(win)
+
+
+    txt3 = Text(Point (280, 460), "(Click to Continue)")
+    txt3.setSize(10)
+    txt3.draw(win)
+
+    while True:
+        point = window.getMouse()
+        px, py = point.getX(), point.getY()
+        if px, py = 
+
+
+starter()
+
+def question1
+    # put the text here, move later
+    q1 = print("Would you marry your cousin?")
+    a1 = print("No, I'd rather be in a hateful marriage.")
+    b1 = print("Only if they give me a son.")
+    c1 = print("Maybe, but they MUST be a Hapsburg.")
+    d1 = print("I NEVER want to get married.")
+    e1 = print("If I kidnapped and forced to, then I guess I have no choice.")
+    
+    def answers1
+        q1text = Text(Point(300,300), a1)
+        q1text = setSize(15)
+        q1text.draw(win)
+
+        recta1 = Rect(Point(
+
+
+def click(x):
+    #for x in range(1000):
+        #click = getClick()
+
+    if x == 1:
+        return 1
+    if x == 2:
+        return 2
+
+number = click(2)
+
+print(number)
+
+George = 0
+Henry = 0
+if number == 1:
+    George += 1
+
+if number == 2:
+    Henry += 1
+
+print(Henry)
+
+
